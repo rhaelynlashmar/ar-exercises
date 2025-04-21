b6b0af61f0f973593cc905134fcda3c87bd15285
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-04-21 14:02:10 -0700

    exercise 2 completed

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,13 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Load the first store and assign it to @store1
+@store1 = Store.find(1)
+
+# Load the second store and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the first store's name
+@store1.update(name: "Metrotown")
+
+puts "Updated store name: #{@store1.name}"
